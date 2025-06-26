1b53b7c888aec55f75bbf2396780a66177114be6
Your Name <you@example.com>
2025-06-26 10:25:40 -0700

    updated

---
+++ server.js
@@ -0,0 +1,16 @@
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("destinations.json");
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+server.use(jsonServer.bodyParser);
+server.use("/bookings", router);
+server.use("/favorites", router);
+server.use("/destinations", router);
+server.use(router);
+
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+  console.log(`JSON Server is running on port ${port}`);
+});
