d133b4ea41c2b0b8eabe7ff99ee8eeb1b94e9887
James-Patrick416 <jp1252211@gmail.com>
2025-06-27 01:31:36 -0700

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,15 @@ const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 server.use(jsonServer.bodyParser);
-server.use(router); // mount once at root
+
+server.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+  next();
+});
+
+server.use(router);
 
 const port = process.env.PORT || 3000;
 server.listen(port, () => {
